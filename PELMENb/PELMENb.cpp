050f21490d77258e0d99a05057e79a5d79408f10
Anfitevien <Anfitevien@gmail.com>
2025-04-28 19:34:16 +0300

    Они теперь и работают!

--- PELMENb/PELMENb.cpp
+++ PELMENb/PELMENb.cpp
@@ -17,16 +17,12 @@ int main()
     cin >> t;
     cout << "af = ";
     cin >> af;
-    cout << "nf = ";
-    cin >> nf;
-    cout << "nt = ";
-    cin >> nt;
+    Qp = M / (2 * t);
     np = Qp / qp;
-    Qp = M / 2 * t;
     Qf = af * Qp;
     nf = Qf / qf;
     Qt = (1 - af) * Qp;
-    nf = Qt / qt;
+    nt = Qt / qt;
     cout << "Количество автоматов для отдела пельменей: " << np << endl;
     cout << "Количество автоматов для отдела фарша: " << nf << endl;
     cout << "Количество автоматов для отдела теста: " << nt << endl;
